100e63cc09a35465d703386e4732edb6ae537c78
Naoto Takai <takai@recompile.net>
2015-11-11 06:25:40 +0900

    Fix for rubocop:cop:

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -9,11 +9,10 @@ describe 'The HelloWorld App' do
   include Rack::Test::Methods
 
   let(:app) { Sinatra::Application }
-  it "returns ip address" do
+  it 'returns ip address' do
     get '/'
     expect(last_response).to be_ok
     expect(last_response.content_type).to eq('text/plain;charset=utf-8')
     expect(last_response.body).to eq('127.0.0.1')
   end
 end
-
